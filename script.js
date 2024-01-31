318b9ece1e6ad79149db1231cd2d6b95d038ffa8
BestChoripans <enderrfer@outlook.com>
2024-01-31 12:31:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function easterEgg() {
 		easterEggClicks++;
 		document.getElementById("music").loop = true;
 		document.getElementById("music").play();
-		document.getElementById("easter-egg").setAttribute("style","color: #00aa00");
+		document.getElementById("easter-egg").setAttribute("style","color: #fe44d1");
 	} else {
 		if (easterEggClicks < 3) {
 			document.getElementById("click").play();
