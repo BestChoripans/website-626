e85743e2a2d1d0b86a60cba4e4060b548c9d66a6
BestChoripans <enderrfer@outlook.com>
2024-01-31 12:22:01 -0300

    xd

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 var easterEggClicks = 0;
 
 function easterEgg() {
-	if (easterEggClicks == 2) {
+	if (easterEggClicks == 1) {
 		document.getElementById("click").play();
-		easterEggClicks++;
 		document.getElementById("music").loop = true;
 		document.getElementById("music").play();
-		document.getElementById("easter-egg").setAttribute("style","color: #00aa00");
+		document.getElementById("easter-egg").setAttribute("style","color: #fe44d1");
 	} else {
 		if (easterEggClicks < 3) {
 			document.getElementById("click").play();
