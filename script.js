a3f1facf64cbe7c365bb2e0898f87477b30808fb
BestChoripans <enderrfer@outlook.com>
2024-02-03 21:31:52 -0300

    xd

--- script.js
+++ script.js
@@ -1,37 +1,16 @@
 var easterEggClicks = 0;
-var panoramaPosition = 0;
-
-if (localStorage.panoramaMovement == undefined) {
-	localStorage.panoramaMovement = "true";
-} else if (localStorage.panoramaMovement == "false") {
-	document.body.style.animation = "none";
-}
 
 function easterEgg() {
-	if (easterEggClicks == 2) {
+	if (easterEggClicks == 0) {
 		document.getElementById("click").play();
 		easterEggClicks++;
 		document.getElementById("music").loop = true;
 		document.getElementById("music").play();
-		document.getElementById("easter-egg").setAttribute("style","color: #00aa00");
+		document.getElementById("easter-egg").setAttribute("style","color: #f700ff");
 	} else {
-		if (easterEggClicks < 3) {
+		if (easterEggClicks < 1) {
 			document.getElementById("click").play();
 		}
 		easterEggClicks++;
-	}
-}
-
-function clickSound() {
-	document.getElementById("click").play();
-}
-
-function triggerPanoramaMovement() {
-	if (localStorage.panoramaMovement == "false") {
-		document.body.style.animation = "";
-		localStorage.panoramaMovement = "true";
-	} else {
-		document.body.style.animation = "none";
-		localStorage.panoramaMovement = "false";
 	}
 }
